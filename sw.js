311cad9f5d1cdb1ce630da264749ac92ac9fa756
tmn2574-cloud <tmn2574@gmail.com>
2026-06-01 10:27:39 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'keibi-liff-attendance-v2';
-const ASSETS = ['./index.html', './manifest.json'];
+﻿const CACHE_NAME = 'keibi-attendance-v5';
+const ASSETS = ['./index.html', './keibi_attendance.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
@@ -29,3 +29,4 @@ self.addEventListener('fetch', e => {
   }
   e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request)));
 });
+
